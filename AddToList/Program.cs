17c920bd7320d2084c0968d5b68b63e6c4abb5bf
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-02 16:02:43 +0200

    Add project files.

---
+++ AddToList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddToList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
